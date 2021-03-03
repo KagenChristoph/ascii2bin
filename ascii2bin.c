d2546c9af01d81b67131628fc7f9367d77af5d31
KagenChristoph <kagen.christoph.322@my.csun.edu>
2021-03-02 16:11:15 -0800

    Initial Version

---
+++ ascii2bin.c
@@ -0,0 +1,35 @@
+/********************************/
+/* Program Name:                */
+/* Author:                      */
+/* Date:                        */
+/********************************/
+/* Description:                 */
+/* Validation Checks:           */
+/* Enhancements:                */
+/********************************/
+
+#include "stdio.h"
+#include "stdlib.h"
+#include <unistd.h>
+
+#define byte char
+
+int main (int argc, char * argv[], char ** envp) {
+
+  int retval;
+  int ascii_value;
+  int retval;
+  int offset = 48;
+  int number = 0;
+    
+    retval = read(0, &ascii_value, 1);
+    while (retval == 1)
+	{
+        int digit = ascii_value - offset;
+        number = (number << 1) + digit;  
+        retval = read(0, &ascii_value, 1);
+    }
+    printf("%u\n", number);
+    return 0;
+
+}
