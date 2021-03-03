2921801674a16dabe84854b6a4f044ca99f0b427
KagenChristoph <78060919+KagenChristoph@users.noreply.github.com>
2021-03-02 21:25:40 -0800

    Version 1

--- ascii2bin.c
+++ ascii2bin.c
@@ -20,22 +20,10 @@ int main (int argc, char * argv[], char ** envp) {
   int ascii_value;
   int offset = 48;
   int number = 0;
-  int count = 0; 
   
     int retval = read(0, &ascii_value, 1);
     while (retval == 1)
 	{
-		count++;
-
-    if (count > 32) {
-      fprintf(stderr, "%s", "Error, number exceeds 2^32!");
-      return 1;
-    }
-
-    if (ascii_value != '0' && ascii_value != '1') {
-      fprintf(stderr, "%s", "Error, invalid input!");
-      return 1;
-    }
         int digit = ascii_value - offset;
         number = (number << 1) + digit;  
         retval = read(0, &ascii_value, 1);
