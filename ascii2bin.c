3d86fee66598497b36cc454cc44c9c8558a94a3e
KagenChristoph <78060919+KagenChristoph@users.noreply.github.com>
2021-03-02 16:30:56 -0800

    Update ascii2bin.c

--- ascii2bin.c
+++ ascii2bin.c
@@ -5,7 +5,7 @@
 int main (int argc, char * argv[], char ** envp)
 {
         char ascii_value = 0;
-        char offset = 0x30;                  //0x30  //48
+        char offset = 0x30;                  //48
         char digit = 0;
         unsigned int number = 0;
         int retval;
@@ -15,20 +15,16 @@ int main (int argc, char * argv[], char ** envp)
             while (retval == 1)
             {
                 if(ascii_value == 0x30 || ascii_value == 0x31) 
-                {                                              
-                    //printf("%d\n", ascii_value);             
+                {                                                          
                     digit = ascii_value - offset;
                     number = (number << 1) + digit;         
                     retval = read(0, &ascii_value, 1);  
                 }
                 else
                 {
-                        retval = 0;    
+                        break;
                 }
-                //printf("%d\n", digit);
-            }
-            //printf("%d\n", ascii_value);
-        
+            }        
     if(ascii_value == 0x30 || ascii_value == 0x31 || ascii_value == 0x0a)
     {
         printf("%u\n", number);
