5c9675b1f9d1487067c95a94c0948ddfce82d98f
KagenChristoph <78060919+KagenChristoph@users.noreply.github.com>
2021-03-02 16:29:24 -0800

    Update ascii2bin.c

--- ascii2bin.c
+++ ascii2bin.c
@@ -1,34 +1,41 @@
-/********************************/
-/* Program Name:                */
-/* Author:                      */
-/* Date:                        */
-/********************************/
-/* Description:                 */
-/* Validation Checks:           */
-/* Enhancements:                */
-/********************************/
-
-#include "stdio.h"
-#include "stdlib.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
-#define byte char
+int main (int argc, char * argv[], char ** envp)
+{
+        char ascii_value = 0;
+        char offset = 0x30;                  //0x30  //48
+        char digit = 0;
+        unsigned int number = 0;
+        int retval;
 
-int main (int argc, char * argv[], char ** envp) {
-
-  int ascii_value;
-  int retval;
-  int offset = 48;
-  int number = 0;
-  
-    retval = read(0, &ascii_value, 1);
-    while (retval == 1)
-	{
-        int digit = ascii_value - offset;
-        number = (number << 1) + digit;  
         retval = read(0, &ascii_value, 1);
+        
+            while (retval == 1)
+            {
+                if(ascii_value == 0x30 || ascii_value == 0x31) 
+                {                                              
+                    //printf("%d\n", ascii_value);             
+                    digit = ascii_value - offset;
+                    number = (number << 1) + digit;         
+                    retval = read(0, &ascii_value, 1);  
+                }
+                else
+                {
+                        retval = 0;    
+                }
+                //printf("%d\n", digit);
+            }
+            //printf("%d\n", ascii_value);
+        
+    if(ascii_value == 0x30 || ascii_value == 0x31 || ascii_value == 0x0a)
+    {
+        printf("%u\n", number);
+        return 0;
     }
-    printf("%u\n", number);
-    return 0;
+    return 1;
+
+
 
 }
