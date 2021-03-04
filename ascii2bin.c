6d5063707a533baf0007725383097ae2db4478bc
KagenChristoph <78060919+KagenChristoph@users.noreply.github.com>
2021-03-04 13:09:16 -0800

    Version 3

--- ascii2bin.c
+++ ascii2bin.c
@@ -12,27 +12,34 @@
 #include <unistd.h>
 
 #define byte unsigned char
+#define max_count (32)
 
 
 int main (int argc, char * argv[], char ** envp)
 {
         byte ascii_value = 0;
-        byte offset = 48; //48
+        byte offset = 48; 
         byte digit = 0;
         unsigned int number = 0;
         int retval;
+	int count = 0;
 
         retval = read(0, &ascii_value, 1);
         
             while (retval == 1)
             {
-				printf("%d\n",ascii_value);
+		if(count > max_count)                                           /  
+        	{                                                               
+            	fprintf(stderr,"%s","The calculated number exceeds 2^32.");
+		return 1;
+        	}
                 if(ascii_value != 48 && ascii_value != 49) // needs to be within range 48-49 
                 {                                                          
                    break;
                 }
                 else
                 {
+		    count++;
 		    digit = ascii_value - offset;
                     number = (number << 1) + digit;         
                     retval = read(0, &ascii_value, 1);  
@@ -43,5 +50,10 @@ int main (int argc, char * argv[], char ** envp)
         printf("%u\n", number);
         return 0;
     }
-    return 1;
+    else
+    {
+	    fprintf(stderr,"%s","A non binary input has been detected");
+	    return 1;
+    }
+    
 }
